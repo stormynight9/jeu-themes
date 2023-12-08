02a509636c257630938dba842a22d687a9ae62f8
stormynight9 <ferjani.nader@hotmail.fr>
2023-12-08 06:10:22 +0100

    Refactor high-temperature component and update temperature interpolation

--- src/components/high-temperature.tsx
+++ src/components/high-temperature.tsx
@@ -1,24 +1,23 @@
 'use client'
 
+import { useEffect, useState } from 'react'
 import { Range } from 'react-range'
 import Land from './icons/land'
-import Sun from './icons/sun'
-import { useEffect, useState } from 'react'
-import { GripIcon } from 'lucide-react'
 import Landx from './icons/landx'
+import Sun from './icons/sun'
 
 const DATA = [
     {
         prevYear: 1850,
         nextYear: 1900,
-        text: 'Les premiers signes du réchauffement climatique',
-        description: `Les premiers signes du réchauffement climatique sont détectés. La température moyenne de la planète a augmenté de 0,6 °C depuis le début de l'ère industrielle. La première conférence internationale sur le changement climatique a lieu à Genève.`,
+        text: 'Les premiers signes du changement climatique',
+        description: `Les premiers signes du changement climatique sont détectés. La température moyenne de la planète a augmenté de 0,6 °C depuis le début de l'ère industrielle. La première conférence internationale sur le changement climatique a lieu à Genève.`,
     },
     {
         prevYear: 1900,
         nextYear: 1950,
         text: "Essor de l'industrialisation et augmentation des émissions",
-        description: `Le début du XXe siècle voit une montée rapide de l'industrialisation, entraînant une augmentation des émissions de gaz à effet de serre. La combustion de combustibles fossiles devient un contributeur majeur à la tendance actuelle du réchauffement climatique.`,
+        description: `Le début du XXe siècle voit une montée rapide de l'industrialisation, entraînant une augmentation des émissions de gaz à effet de serre. La combustion de combustibles fossiles devient un contributeur majeur à la tendance actuelle du changement climatique.`,
     },
     {
         prevYear: 1950,
@@ -34,9 +33,28 @@ const DATA = [
     },
 ]
 
+function interpolate(value: number) {
+    const inputMin = 0
+    const inputMax = 100
+    const outputMin = 13.7
+    const outputMax = 15.7
+
+    const result =
+        outputMin +
+        (outputMax - outputMin) * ((value - inputMin) / (inputMax - inputMin))
+    return result
+}
+
 const HighTemperature = () => {
     const [values, setValues] = useState([1850])
     const [percentage, setPercentage] = useState(0)
+    const [averateTemperature, setAverageTemperature] = useState(0)
+
+    useEffect(() => {
+        setAverageTemperature(interpolate(percentage))
+    }, [percentage])
+
+    console.log(averateTemperature)
 
     useEffect(() => {
         setPercentage((values[0] - 1850) / 2)
@@ -52,17 +70,23 @@ const HighTemperature = () => {
                 renderTrack={({ props, children }) => (
                     <div
                         {...props}
-                        className='mx-auto mt-8 h-4 w-full max-w-5xl rounded-full bg-gradient-to-r from-green-500 via-yellow-500 to-red-600'
+                        className='relative mx-auto mt-8 h-4 w-full max-w-5xl rounded-full bg-gradient-to-r from-green-500 via-yellow-500 to-red-600'
                     >
                         {children}
+                        <p className='absolute -right-60 -top-1 text-lg'>
+                            Temperature moyone:{' '}
+                            <span className=' font-bold'>
+                                {averateTemperature.toFixed(2)} °C
+                            </span>
+                        </p>
                     </div>
                 )}
                 renderThumb={({ props }) => (
                     <div
                         {...props}
-                        className='absolute flex h-6 w-6 items-center justify-center rounded-full border-none bg-white shadow-lg'
+                        className='absolute flex h-6 items-center justify-center rounded-full border-none bg-white px-4 shadow-lg'
                     >
-                        <GripIcon className='h-4 w-4' />
+                        Glisser
                     </div>
                 )}
             />
